9607d50cc17251f01e8725f0c72569fa98e75012
DerekYn <33822447+DerekYn@users.noreply.github.com>
2018-05-28 17:49:43 +0900

    Add files via upload

---
+++ myshop/controller/LikeAddAction.java
@@ -0,0 +1,53 @@
+package myshop.controller;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.JSONObject;
+
+import common.controller.AbstractController;
+import myshop.model.InterProductDAO;
+import myshop.model.ProductDAO;
+
+public class LikeAddAction extends AbstractController {
+
+	@Override
+	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		
+		String userid = req.getParameter("userid");
+		String pnum = req.getParameter("pnum");
+		
+		JSONObject jsonObj = new JSONObject();
+		
+		if("".equals(userid))  // if(userid == null) 이 아니다.
+			jsonObj.put("msg", "로그인을 하신 후 \n 좋아요를 클릭하세요.");
+		else {
+			InterProductDAO pdao = new ProductDAO();
+			
+			try {
+				
+				int n = pdao.insertLike(userid, pnum);
+				
+				if(n>0)
+					jsonObj.put("msg", "해당제품에 \n 좋아요를 클릭하셨습니다.");
+				
+			} catch (SQLIntegrityConstraintViolationException e) {
+				jsonObj.put("msg", "이미 좋아요를 선택한 상품입니다!!\n 두번의 좋아요는 불가능합니다.");
+			}
+			
+		}// end of if~else------------------------------------------
+		
+		String str_jsonObj = jsonObj.toString();
+		
+		req.setAttribute("str_jsonObj", str_jsonObj);
+			
+		super.setViewPage("/WEB-INF/myshop/likeAdd.jsp");
+		
+		
+		
+
+	}
+
+}
